7c4a2e486b852703f926a71ae71b6dfdb67f21ec
David Kim <kimbyi@bc.edu>
2020-03-21 22:49:37 -0400

    database stuff

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'david kim',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'August 27, 2019'
-    }, 
-    {
-        'author': 'king dawei',
-        'title': 'Blog Post 2',
-        'content': 'Second post',
-        'date_posted': 'August 28, 2019'
-    }
+# posts = [
+#     {
+#         'author': 'david kim',
+#         'title': 'Blog Post 1',
+#         'content': 'First post',
+#         'date_posted': 'August 27, 2019'
+#     }, 
+#     {
+#         'author': 'king dawei',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post',
+#         'date_posted': 'August 28, 2019'
+#     }
+# ]
 
-]
 def home(request):
     context = {
-        'posts': posts
+        #'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
