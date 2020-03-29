a01094b1ac52ea326c152f306545a5a061831b84
David Kim <kimbyi@bc.edu>
2020-03-28 21:01:46 -0400

    Email Password Reset (Error with Admin)

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -129,3 +129,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'swoopo0716@gmail.com'
+EMAIL_HOST_PASSWORD = 'z9e5ru48380512'   
