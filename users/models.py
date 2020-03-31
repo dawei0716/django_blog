10173fc61068272d7dc94ed9bbd3b6d1896fecda
David Kim <kimbyi@bc.edu>
2020-03-31 02:21:19 -0400

    AWS S3

--- users/models.py
+++ users/models.py
@@ -11,8 +11,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+
+    #NOT FOR AWS S3
+            
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
         # img = Image.open(self.image.path)
         # if img.height > 300 or img.width > 300:
