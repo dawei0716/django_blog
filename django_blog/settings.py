7e5ecd7b0c35a3f36204d050d303f5ab86e24cfd
David Kim <kimbyi@bc.edu>
2020-04-01 01:35:30 -0400

    bug fix attempt

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'swoopo0716@gmail.com'
-EMAIL_HOST_PASSWORD = 'z9e5ru0716'   
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
 
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
